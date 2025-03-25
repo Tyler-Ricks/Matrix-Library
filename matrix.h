64fe43c74d45b502dcab27c7e6bd983072e99331
Tyler Ricks <rickstd@vcu.edu>
2025-03-25 09:01:51 -0400

    changed the big transpose comment in matrix.h

--- matrix.h
+++ matrix.h
@@ -9,30 +9,32 @@
 
 #include "memoryPool.h"
 
-// both macros check m from fmatrix for if it's negative. If it is, then treat mat as a transpose
-// macro so I can just use a 1D array for matrix struct
-#define OLD_MATRIX_AT(mat, i, j) (mat.matrix[i * mat.n + j])
+// both macros check fmatrix transpose flag. If it's set, then treat mat as a transpose
+// gets the element of the matrix at mat[i][j]
 #define MATRIX_AT(mat, i, j) ((mat.transpose) ? (mat.matrix[j * mat.m + i]) : (mat.matrix[i * mat.n + j]))
 // macro to get array index given matrix index
-#define OLD_INDEX_AT(mat, i, j) (i * mat.n + j) 
 #define INDEX_AT(mat, i, j) ((mat.transpose) ? (j * mat.m + i) : (i * mat.n + j))
+
+// old implementation before transpose flag
+#define OLD_MATRIX_AT(mat, i, j) (mat.matrix[i * mat.n + j])
+#define OLD_INDEX_AT(mat, i, j) (i * mat.n + j) 
+
 // error matrix
 # define ERROR_FMATRIX (fmatrix){ 0, 0, NULL, 0 }
  /*
 // I have a really strange idea that I want to work with later.
-// I'll use the transpose operation as an example. Currently, it takes O(mn), because we
+// I'll use the transpose operation as an example. normally, it takes O(mn), because we
 // either copy the contents of the matrix in to another one, or rearrange elements from 
 // the input matrix.
 // However, I have an idea that could also make implementing column operations not as
 // tedious.
-// If I somehow include in the fmatrix struct a way to read from the matrix, transpose 
-// could be done in constant time. Transpose basically turns the matrix's rows into
+// transpose could be done in constant time, by having a flag indicating a transpose in
+// the struct. transpose in real life basically turns the matrix's rows into
 // columns and columns into rows. In other words, we change how to read from the matrix
 // from row major order into column major order. 
-// This is how I want to make this work in
-// constant time. All it would take to transpose a matrix is to change the method we use
-// to read the matrix. (It would still take O(mn) time for not in-place algorithms so 
-// the matrix contents get copied into the new matrix)
+// This is how I want to make this work in constant time. All it would take to transpose
+// a matrix is to change the method we use to read the matrix. (It would still take O(mn)
+// time for not in-place algorithms so the matrix contents get copied into the new matrix)
 
 // This also can generalize row operations and column operations into the same operation.
 // A column operation can be done by just transposing a matrix, doing a row operation, 
@@ -41,13 +43,13 @@
 // Of course, implementing it as a direct column operation is a little faster, but I 
 // still like this trick
 
-// To indicate that a matrix should be read in column major order, I'll inc;ude a flag in 
+// To indicate that a matrix should be read in column major order, I'll include a flag in 
 // the fmatrix struct. 
 
 // CHECKLIST:
 // (done) 1) Refactor the macros to support this idea 
-//		  5) Make sure matrix multiplication works when multiplying two transposes
-//		  3) There's some spots where I opted to not use the macros to remove redundancy. These
+// (done) 5) Make sure matrix multiplication works when multiplying two transposes
+// (done) 3) There's some spots where I opted to not use the macros to remove redundancy. These
 //				Will need to be fixed.
 //		  4) Refactor matrix multiplication to check for transposes for better cache performance
 //				(Do it in a column-major accumulation process so we aren't jumping around the array)
@@ -73,8 +75,6 @@ void print_fpool(pool *frame);
 void print_properties(fmatrix mat);
 void print_as_array(fmatrix mat);
 fmatrix fmatrix_copy_alloc(fmatrix mat, pool *frame);
-int fmatrix_get_m(fmatrix mat);
-int fmatrix_get_n(fmatrix mat);
 
 void fswap(float *a, float *b);
 void intswap(int *a, int *b);
